adaa9f9249798b1f60b319e83f8ffd09710177a3
Muh. Naufal Febriantama <naufal77febriantama@gmail.com>
2021-12-25 13:38:10 +0800

    logo kominfo

--- resources/views/loginViews/login.blade.php
+++ resources/views/loginViews/login.blade.php
@@ -19,7 +19,7 @@
   <!-- inject:css -->
   <link rel="stylesheet" href="{{ asset('')}}assets/admin/css/vertical-layout-light/style.css">
   <!-- endinject -->
-  <link rel="shortcut icon" href="{{ asset('')}}assets/admin/images/favicon.png" />
+  <link rel="shortcut icon" href="{{ asset('')}}assets/admin/images/logo.png" />
 </head>
 <body>
   <div class="container-scroller">
